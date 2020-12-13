b5588112502c24151054c2264cef39ee9529e00f
Marko <markod.505@gmail.com>
2020-12-13 16:50:24 +0100

    Add categories.sql file.

--- test.php
+++ test.php
@@ -3,27 +3,27 @@
 require 'config.php';
 
 //Dobavljanje svih kategorija:
-// $allCategories = Category::getAll("where id in (1,3,5)");
-// print_r($allCategories);
+echo "<pre>";
+$allCategories = Category::getAll("where id in (1,3,5)");
+print_r($allCategories);
+echo "</pre>";
 
 // Izmena rezultata iz baze:
 // $cat = Category::get(1);
-// // print_r($cat);
-// $cat->name = "123";
+// print_r($cat);
+// $cat->name = "Action";
+// $cat->description ="Action & Advanture";
 // $cat->save(); //Uciniti izmenu prezistentnom
 // print_r($cat);
 
 // Ubacivanje u bazu podataka:
-
 // $cat = new Category;
 // $cat->name = "My new category";
 // $cat->description = "Some description";
 // $cat->insert();
 
 
-/*
-$cat = new Category;
-$cat->delete(9);
-*/
+// Brisanje kategorije iz baze podataka:
+// $cat = new Category;
+// $cat->delete(13);
 
-//print_r($cat);
